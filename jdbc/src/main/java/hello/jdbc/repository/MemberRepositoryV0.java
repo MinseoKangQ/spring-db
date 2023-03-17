af79e458778128122a3770d6bc23a74ddd5edff9
MinseoKangQ <kms02171@naver.com>
2023-03-17 18:24:30 +0900

    Edit MemberRepositoryV0 class to simple

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -1,18 +1,17 @@
 package hello.jdbc.repository;
 
-
 // JDBC : DriverManger 사용
 
 import hello.jdbc.connection.DBConnectionUtil;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
-
 import java.sql.*;
 
 @Slf4j
 public class MemberRepositoryV0 {
 
     public Member save(Member member) throws SQLException{
+        
         String sql = "insert into member(member_id, money) values (?, ?)";
 
         Connection con = null;
@@ -28,8 +27,7 @@ public class MemberRepositoryV0 {
         } catch (SQLException e) {
             log.error("db error", e);
             throw e;
-        } finally { // 연결된 역순으로 close 해야함
-            // 닫아주는 것은 보장되어야 하기 때문에 finally에서 해야함
+        } finally {
             close(con, pstmt, null);
         }
     }
@@ -46,7 +44,7 @@ public class MemberRepositoryV0 {
 
         if(stmt != null) {
             try {
-                stmt.close(); // SQL Exception
+                stmt.close();
             } catch (SQLException e) {
                 log.info("error", e);
             }
@@ -54,7 +52,7 @@ public class MemberRepositoryV0 {
 
         if(con != null) {
             try {
-                con.close(); // SQL Exception
+                con.close();
             } catch (SQLException e) {
                 log.info("error", e);
             }
