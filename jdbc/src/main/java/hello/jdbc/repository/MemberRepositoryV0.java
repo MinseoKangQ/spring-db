d417229e0e3fcd0d43e51c581a68181f192c70ab
MinseoKangQ <kms02171@naver.com>
2023-03-17 19:22:11 +0900

    Write findById method in MemberRepositoryV0 class, and findById test code in MemberRepositoryV0Test class

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,13 +5,17 @@ package hello.jdbc.repository;
 import hello.jdbc.connection.DBConnectionUtil;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.xml.transform.Result;
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 @Slf4j
 public class MemberRepositoryV0 {
 
+    // C
     public Member save(Member member) throws SQLException{
-        
+
         String sql = "insert into member(member_id, money) values (?, ?)";
 
         Connection con = null;
@@ -29,6 +33,39 @@ public class MemberRepositoryV0 {
             throw e;
         } finally {
             close(con, pstmt, null);
+        }
+    }
+
+    // R
+    public Member findById(String memberId) throws SQLException {
+
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery(); // ResultSet 반환, 값을 꺼내야 함
+            if(rs.next()) { // next를 한 번은 호출해야 함, 데이터가 있는 경우
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }
+            else { // 데이터가 없는 경우
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
         }
     }
 
--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,28 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
     MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+
+        // save
         Member member = new Member("memberV1", 10000);
         repositoryV0.save(member);
-        System.out.println(member);
+
+        // findById
+        Member findMember = repositoryV0.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
